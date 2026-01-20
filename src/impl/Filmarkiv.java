f340c3996e7772407e7c90aff524b353966782c7
endretysnes <entys0795@hvl.no>
2026-01-20 13:02:11 +0100

    Update Filmarkiv.java

--- src/impl/Filmarkiv.java
+++ src/impl/Filmarkiv.java
@@ -1,4 +1,40 @@
 package impl;
 
-public class Filmarkiv {
+import adt.FilmarkivADT;
+
+public class Filmarkiv implements FilmarkivADT {
+    @Override
+    public Film finnFilm(int nr) {
+        return null;
+    }
+
+    @Override
+    public void leggTilFilm(Film nyFilm) {
+
+    }
+
+    @Override
+    public boolean slettFilm(int filmnr) {
+        return false;
+    }
+
+    @Override
+    public Film[] soekTittel(String delstreng) {
+        return new Film[0];
+    }
+
+    @Override
+    public Film[] soekProdusent(String delstreng) {
+        return new Film[0];
+    }
+
+    @Override
+    public int antall(Sjanger sjanger) {
+        return 0;
+    }
+
+    @Override
+    public int antall() {
+        return 0;
+    }
 }

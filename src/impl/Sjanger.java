0388b24f2a745189c26ee5b5938d948813e0c8ef
simulert <gunnargustad@hotmail.com>
2026-01-21 09:00:13 +0100

    film,sjanger

--- src/impl/Sjanger.java
+++ src/impl/Sjanger.java
@@ -1,4 +1,17 @@
 package impl;
 
 public enum Sjanger {
+	ACTION, ADVENTURE, COMEDY, 
+	DOCUMENTARY, DRAMA, FANTASY, 
+	HORROR, REALITY, SCIFI;
+	
+	public static Sjanger sok(String sjanger){
+		for (Sjanger s : Sjanger.values()){
+			if (s.toString().equals(sjanger.toUpperCase())){
+				return s;
+			}
+		}
+		// hvis sjangeren ikke er listet
+		return null;
+	}
 }

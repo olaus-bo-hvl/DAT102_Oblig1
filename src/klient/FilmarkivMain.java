d87b6ab33f6d7006acdb4fbd978e836ea77bff7d
simulert <gunnargustad@hotmail.com>
2026-01-21 12:37:50 +0100

    ---

--- src/klient/FilmarkivMain.java
+++ src/klient/FilmarkivMain.java
@@ -1,4 +1,7 @@
 package klient;
 
 public class FilmarkivMain {
+	public static void main(String[] args){
+		
+	}
 }
--- src/klient/Meny.java
+++ src/klient/Meny.java
@@ -1,4 +1,14 @@
 package klient;
 
-public class Meny {
+import java.util.Scanner;
+
+public class Meny{
+	public Meny(){
+		Scanner ska = new Scanner(System.in);
+		String spor = "";
+		while (!spor.equals("s")){
+			System.out.println("hva onsker du a gjore?");
+			spor = ska.nextLine();
+		}
+	}
 }
